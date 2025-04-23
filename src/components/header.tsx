7777fb384eb34e6def92e07381425e90b191590d
Thiago Baptista <thiago@syncmob.com.br>
2025-04-22 22:58:20 -0300

    Cart UI updates

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -82,82 +82,80 @@ export default function Header() {
           Continue shopping
         </div>
         <div className={`${block}__cart-drawer-container`}>
-          {cartContext.items.length === 0 ? (
-            <div className={`${block}__cart-drawer-empty`}>
-              Your cart is empty
-            </div>
-          ) : (
-            <div className={`${block}__cart-drawer-items`}>
-              {cartContext.items.map((item, index) => (
-                <div
-                  key={index}
-                  className={`${block}__cart-drawer-item-container`}
-                >
-                  <div className={`${block}__cart-drawer-item`}>
-                    <div className={`${block}__cart-drawer-item-title`}>
-                      {item.label || `Unnamed Build`}
-                    </div>
-                    <div className={`${block}__cart-drawer-item-price`}>
-                      ${item.price}
-                    </div>
-                  </div>
-                  <div className={`${block}__cart-drawer-item-parts`}>
-                    {item.parts &&
-                      item.parts.map((part: any, index: number) => (
-                        <div
-                          className={`${block}__cart-drawer-item-part`}
-                          key={index}
-                        >
-                          <div
-                            className={`${block}__cart-drawer-item-part-label`}
-                          >
-                            {part.label}&nbsp;
-                            <span className="lowercase">
-                              {part.category_label}
-                            </span>
-                          </div>
-                          <div
-                            className={`${block}__cart-drawer-item-part-price`}
-                          >
-                            $
-                            {formatAsDollar(
-                              part.customPrice || part.price || 0
-                            )}
-                          </div>
-                        </div>
-                      ))}
-                  </div>
-                  <button
-                    onClick={() => cartContext.remove(item)}
-                    className={`${block}__cart-drawer-item-remove`}
+          <div className={`${block}__cart-drawer-container-inner container`}>
+            {cartContext.items.length === 0 ? (
+              <div className={`${block}__cart-drawer-empty`}>
+                Your cart is empty
+              </div>
+            ) : (
+              <div className={`${block}__cart-drawer-items`}>
+                {cartContext.items.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`${block}__cart-drawer-item-container`}
                   >
-                    Remove
+                    <div className={`${block}__cart-drawer-item`}>
+                      <div className={`${block}__cart-drawer-item-title`}>
+                        {item.label || `Unnamed Build`}
+                      </div>
+                      <div className={`${block}__cart-drawer-item-price`}>
+                        ${item.price}
+                      </div>
+                    </div>
+                    <div className={`${block}__cart-drawer-item-parts`}>
+                      {item.parts &&
+                        item.parts.map((part: any, index: number) => (
+                          <div
+                            className={`${block}__cart-drawer-item-part`}
+                            key={index}
+                          >
+                            <div
+                              className={`${block}__cart-drawer-item-part-label`}
+                            >
+                              {part.label}&nbsp;
+                              <span className="lowercase">
+                                {part.category_label}
+                              </span>
+                            </div>
+                            <div
+                              className={`${block}__cart-drawer-item-part-price`}
+                            >
+                              $
+                              {formatAsDollar(
+                                part.customPrice || part.price || 0
+                              )}
+                            </div>
+                          </div>
+                        ))}
+                    </div>
+                    <button
+                      onClick={() => cartContext.remove(item)}
+                      className={`${block}__cart-drawer-item-remove`}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                ))}
+                <div className={`${block}__cart-drawer-actions`}>
+                  <button
+                    className={`${block}__cart-drawer-checkout-button button`}
+                    onClick={handleCheckout}
+                    data-black-yellow
+                  >
+                    Proceed to Checkout - $
+                    {formatAsDollar(cartContext.totalPrice)}
+                  </button>
+                  <button
+                    className={`${block}__cart-drawer-clear-button button`}
+                    onClick={handleClearCart}
+                    data-yellow-black
+                  >
+                    Clear Cart
                   </button>
                 </div>
-              ))}
-              <div className={`${block}__cart-drawer-total`}>
-                <div className={`${block}__cart-drawer-total-label`}>Total</div>
-                <div className={`${block}__cart-drawer-total-price`}>
-                  ${formatAsDollar(cartContext.totalPrice)}
-                </div>
               </div>
-
-              <div className={`${block}__cart-drawer-actions`}>
-                <button
-                  className={`${block}__cart-drawer-checkout-button`}
-                  onClick={handleCheckout}
-                >
-                  Proceed to Checkout
-                </button>
-                <button
-                  className={`${block}__cart-drawer-clear-button`}
-                  onClick={handleClearCart}
-                >
-                  Clear Cart
-                </button>
-              </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
